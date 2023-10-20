628609bd39aa04aaae23b20ee718a42185634520
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-10-20 15:48:32 +0200

    Removed deado code

--- src/Domain/Strava/MonthlyStatistics.php
+++ src/Domain/Strava/MonthlyStatistics.php
@@ -100,9 +100,6 @@ final class MonthlyStatistics
         $statistics = array_filter($statistics, fn (array $statistic) => $statistic['numberOfRides'] > 0);
 
         foreach ($statistics as &$statistic) {
-            if (0 == $statistic['movingTime']) {
-                continue;
-            }
             $statistic['movingTime'] = CarbonInterval::seconds($statistic['movingTime'])->cascade()->forHumans(['short' => true, 'minimumUnit' => 'minute']);
         }
 
