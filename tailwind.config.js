c14e453c45ac6fd1fafa9bacda86a68325ea2885
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-10-24 19:58:57 +0200

    Better layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     darkMode: 'class',
     content: [
         "./build/html/*.html",
-        "./build/html/**/*.html",
+        "./build/html/activity/activity-8848645176.html",
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
