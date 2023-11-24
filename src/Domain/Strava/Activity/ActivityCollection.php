fede4b188f1a15a028e3c0fe71ca519789ee2275
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-11-24 17:48:18 +0100

    ActivityCollection abstraction

--- src/Domain/Strava/Activity/ActivityCollection.php
+++ src/Domain/Strava/Activity/ActivityCollection.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Strava\Activity;
 
 use App\Infrastructure\ValueObject\Collection;
+use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 
 /**
  * @extends Collection<Activity>
@@ -14,5 +15,18 @@ final class ActivityCollection extends Collection
     public function getItemClassName(): string
     {
         return Activity::class;
+    }
+
+    public function getFirstActivityStartDate(): SerializableDateTime
+    {
+        $startDate = new SerializableDateTime();
+        foreach ($this as $activity) {
+            if ($activity->getStartDate()->isAfterOrOn($startDate)) {
+                continue;
+            }
+            $startDate = $activity->getStartDate();
+        }
+
+        return $startDate;
     }
 }
