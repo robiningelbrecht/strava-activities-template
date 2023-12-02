0ddbc9cd6be235f64d725343980b01ac1411d113
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-12-02 18:24:37 +0100

    Format number

--- src/Infrastructure/Twig/FormatNumberTwigExtension.php
+++ src/Infrastructure/Twig/FormatNumberTwigExtension.php
@@ -4,8 +4,12 @@ namespace App\Infrastructure\Twig;
 
 final readonly class FormatNumberTwigExtension
 {
-    public static function doFormat(float $number, int $precision): string
+    public static function doFormat(?float $number, int $precision): string
     {
+        if (is_null($number)) {
+            return 0;
+        }
+
         return number_format(round($number, $precision), $precision, '.', ' ');
     }
 }
