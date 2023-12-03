b775755f94e68af8ef4076039039767be75977e0
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-12-03 15:29:43 +0100

    Weekly distances chart

--- src/Domain/Strava/Activity/BuildWeeklyDistanceChart/WeeklyDistanceChartBuilder.php
+++ src/Domain/Strava/Activity/BuildWeeklyDistanceChart/WeeklyDistanceChartBuilder.php
@@ -65,11 +65,7 @@ final class WeeklyDistanceChartBuilder
         $xAxisLabels = [];
         /** @var Week $week */
         foreach ($weeks as $week) {
-            if (!$this->useDataZoom && $week == $weeks->getFirst()) {
-                $xAxisLabels[] = '';
-                continue;
-            }
-            if (in_array($week->getLabel(), $xAxisLabels)) {
+            if ($week == $weeks->getFirst() || in_array($week->getLabel(), $xAxisLabels)) {
                 $xAxisLabels[] = '';
                 continue;
             }
