3695960c8b2cfa01440c46f8d4b94141a6d759a8
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-12-18 15:39:17 +0100

    Made a whoopsie

--- src/Domain/Strava/Activity/Stream/StreamType.php
+++ src/Domain/Strava/Activity/Stream/StreamType.php
@@ -5,9 +5,6 @@ namespace App\Domain\Strava\Activity\Stream;
 enum StreamType: string
 {
     case WATTS = 'watts';
-    case ALTITUDE = 'altitude';
     case HEART_RATE = 'heartrate';
-    case LAT_LNG = 'latlng';
     case CADENCE = 'cadence';
-    case VELOCITY = 'velocity_smooth';
 }
