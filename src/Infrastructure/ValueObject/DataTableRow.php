690e4d623d76af682b16159919cb2d7b37e8a7a5
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-01-06 20:33:35 +0100

    Little code optimizations

---
+++ src/Infrastructure/ValueObject/DataTableRow.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\ValueObject;
+
+final readonly class DataTableRow implements \JsonSerializable
+{
+    private function __construct(
+        private string $markup,
+        /** @var string[] */
+        private array $searchables,
+        /** @var array<string, string|int> */
+        private array $sortValues,
+    ) {
+    }
+
+    /**
+     * @param string[] $searchables
+     */
+    public static function create(
+        string $markup,
+        array $searchables,
+        array $sortValues,
+    ): self {
+        return new self(
+            markup: $markup,
+            searchables: $searchables,
+            sortValues: $sortValues
+        );
+    }
+
+    public function getMarkup(): string
+    {
+        return $this->markup;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSearchables(): array
+    {
+        return $this->searchables;
+    }
+
+    /**
+     * @return array<string, string|int>
+     */
+    public function getSortValues(): array
+    {
+        return $this->sortValues;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'active' => true,
+            'searchables' => implode(' ', $this->getSearchables()),
+            'sort' => $this->getSortValues(),
+            'markup' => $this->getMarkup(),
+        ];
+    }
+}
