219e230d1352561f57c8dcb62a6606535f38b9f3
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-01-13 20:56:39 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     darkMode: 'class',
     content: [
         "./build/html/*.html",
-        "./build/html/activity-data-table.json",
-        "./build/html/segment-data-table.json",
+        "./build/html/fetch-json/activity-data-table.json",
+        "./build/html/fetch-json/segment-data-table.json",
         "./build/html/**/activity-8837881433.html",
         "./build/html/month/*.html",
         "./node_modules/flowbite/**/*.js"
