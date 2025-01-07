8233deb5a281c93116aadd497989d569af58607b
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-01-07 09:28:29 +0100

    This is now unsupported

--- src/Console/BuildStravaActivityFilesConsoleCommand.php
+++ src/Console/BuildStravaActivityFilesConsoleCommand.php
@@ -2,16 +2,8 @@
 
 namespace App\Console;
 
-use App\Domain\Strava\Activity\BuildActivityHeatmapChart\BuildActivityHeatmapChart;
-use App\Domain\Strava\Activity\BuildDaytimeStatsChart\BuildDaytimeStatsChart;
-use App\Domain\Strava\Activity\BuildEddingtonChart\BuildEddingtonChart;
-use App\Domain\Strava\Activity\BuildLatestStravaActivities\BuildLatestStravaActivities;
-use App\Domain\Strava\Activity\BuildWeekdayStatsChart\BuildWeekdayStatsChart;
-use App\Domain\Strava\Activity\BuildWeeklyDistanceChart\BuildWeeklyDistanceChart;
-use App\Domain\Strava\Activity\BuildYearlyDistanceChart\BuildYearlyDistanceChart;
 use App\Domain\Strava\BuildHtmlVersion\BuildHtmlVersion;
 use App\Domain\Strava\BuildReadMe\BuildReadMe;
-use App\Domain\Strava\CopyDataToReadDatabase\CopyDataToReadDatabase;
 use App\Domain\Strava\MaxResourceUsageHasBeenReached;
 use App\Infrastructure\CQRS\CommandBus;
 use App\Infrastructure\Time\ResourceUsage\ResourceUsage;
@@ -40,21 +32,6 @@ final class BuildStravaActivityFilesConsoleCommand extends Command
         }
         $this->resourceUsage->startTimer();
 
-        $this->commandBus->dispatch(new CopyDataToReadDatabase($output));
-        $output->writeln('Building latest activities...');
-        $this->commandBus->dispatch(new BuildLatestStravaActivities());
-        $output->writeln('Building weekly distance chart...');
-        $this->commandBus->dispatch(new BuildWeeklyDistanceChart());
-        $output->writeln('Building weekday stats chart...');
-        $this->commandBus->dispatch(new BuildWeekdayStatsChart());
-        $output->writeln('Building daytime stats chart...');
-        $this->commandBus->dispatch(new BuildDaytimeStatsChart());
-        $output->writeln('Building activity heatmap chart...');
-        $this->commandBus->dispatch(new BuildActivityHeatmapChart());
-        $output->writeln('Building Eddington chart...');
-        $this->commandBus->dispatch(new BuildEddingtonChart());
-        $output->writeln('Building yearly distance chart..');
-        $this->commandBus->dispatch(new BuildYearlyDistanceChart());
         $output->writeln('Building README...');
         $this->commandBus->dispatch(new BuildReadMe());
         $output->writeln('Building HTML...');
